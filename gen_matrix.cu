9831f552a8d4bd5d744dab90469f3874b9da2632
Michael Sherman <msherman@winlab.rutgers.edu>
2017-04-18 02:43:32 -0400

    using class for matrix, debug memory corruption

--- gen_matrix.cu
+++ gen_matrix.cu
@@ -1,52 +1,65 @@
 #include <stdlib.h> //for rand
 #include <iostream> // for cout
 #include <vector>  //for vector
+using std::cout;
 
 int DIMX_LIM = 10;
 int DIMY_LIM = 10;
 int MAT_COUNT = 100;
 
+int SEED = 10; //seed for rand
+
 //generate random sized matrices, add pointer to each, to vector
+//template <typename T> //handle multiple types
+class matrix {
+public:
+    int row; //number of rows, y
+    int col; //number of columns, x
+    std::vector<int> data;
+
+    matrix(int columns, int rows) :
+        col(columns), row(rows),
+        data(col * row)
+        {}
+
+    int& getdata(int y, int x){
+       return data[y * col + x]; //vertical position * row length + pos in row
+    };
+};
 
 
 int main(){
 
-    int* mat[MAT_COUNT]; //pointer to pointer to int
+    srand(SEED); //init random gen
+
+    matrix* mat[MAT_COUNT]; //pointer to pointer to int
 
     for(int z = 0; z < MAT_COUNT; z++){
 
-        int dimx = DIMX_LIM;
-        int dimy = DIMY_LIM;
+        int dimx = rand()%DIMX_LIM + 1; //random between 1 and limit
+        int dimy = rand()%DIMY_LIM + 1;
 
-        mat[z] = (int *)malloc(sizeof(int) * dimx * dimy);
-        std::cout << mat[z] <<" ";
-
-//        int *mat_tmp = mat[z]; //must specify size here
-//        std::cout << mat_tmp << "\n";
-
-        for(int i = 0; i < dimx; i++){
-            for(int j = 0; j < dimy; j++){
-                (int(*)[dimx][dimy])(mat[z])[i][j] = 1;
+        mat[z] = new matrix(dimx,dimy); //dimx columns, dimy rows
+        for(int x = 0; x<dimx; x++){
+            for(int y = 0; y<dimy; y++){
+                mat[z]->getdata(x,y) = 5;
             }
-        } 
-//         
-//
-//        for(int i = 0; i < dimx; i++){
-//            for(int j = 0; j < dimy; j++){
-//                std::cout << mat_tmp[i][j] << " ";
-//            }
-//            std::cout << "\n";
-//        }
+        }
     }
-
-
-
-
-
-
-
-
-
-
+    
+    for(int z = 0; z < MAT_COUNT; z++){
+        int dimxn = mat[z]->col;
+        int dimyn = mat[z]->row;
+        std::cout << dimxn <<" ";
+        std::cout << dimyn <<" ";
+        std::cout << "\n";
+        for(int x = 0; x<dimxn; x++){
+            for(int y = 0; y<dimyn; y++){
+                cout << mat[z]->getdata(x,y) << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
 
 };
