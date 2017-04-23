3fd3fff6838ebe215e55de6dc1fa835763ec438f
Michael Sherman <msherman@winlab.rutgers.edu>
2017-04-23 02:19:27 -0400

    single threaded matrix multiplication on gpu TODO currently does memory copy on each iteration

--- gen_matrix.cu
+++ gen_matrix.cu
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-int DIM_LIM = 100;
-int MAT_COUNT = 10;
+int DIM_LIM = 20;
+int MAT_COUNT = 3;
 int SEED = 10; //seed for rand
+    
+
 
 class matrix {
 public:
@@ -10,7 +12,7 @@ public:
     int col; //number of columns, x
     double* data;
     
-    matrix(int columns, int rows) :
+    __host__ __device__ matrix(int columns, int rows) :
         col(columns), row(rows),
         data(new double[col * row])
         {}
@@ -19,7 +21,6 @@ public:
        return data[y * col + x]; //vertical position * row length + pos in row
     };
 };
-
 
 __global__ void d_printMat(matrix *mat)
 {   
@@ -48,6 +49,29 @@ __host__ void printMat(matrix *mat)
         printf("\n");
 }
 
+__global__ void mat_mult(matrix *a, matrix *b, matrix *ans){
+    if(a->row == b->col){
+    
+        int iter = a->row; //number of mults needed
+        printf("a(%d, %d) b(%d,%d)\n",a->col,a->row,b->col,b->row);
+        //ans = new matrix(a->col, b->row);
+        printf("result %d rows %d cols\n", ans->row, ans->col);
+
+        for(int x = 0; x < ans->col; x++){
+            for(int y = 0; y < ans->row; y++){
+                ans->getdata(x,y) = 0; //initialize
+                for(int z = 0; z < iter; z++){
+                    ans->getdata(x,y) += (a->getdata(x,y) * b->getdata(y,x));
+                }
+                //printf("value at %d %d is %f\n", x,y, ans->getdata(x,y));
+            }
+        }
+    }
+    else{
+        printf("matrix size mismatch");
+    }
+};
+
 matrix** initialize(){
     srand(SEED); //init random gen
     int dim[MAT_COUNT + 1]; //stores matrix sizes
@@ -73,25 +97,57 @@ matrix** initialize(){
     return mat;
 }
 
+matrix* copyMatrixDev(matrix *host){
+        matrix *d_mat;
+        double *tmp_data;
+        cudaMalloc(&d_mat, sizeof(matrix));
+        cudaMemcpy(d_mat, host, sizeof(matrix),
+                cudaMemcpyHostToDevice);
+        cudaMalloc(&tmp_data, sizeof(double) * host->col * host->row);
+        cudaMemcpy(tmp_data, host->data, sizeof(double) * host->col * host->row,
+                cudaMemcpyHostToDevice);
+        cudaMemcpy(&(d_mat->data),&tmp_data, sizeof(double *),
+                cudaMemcpyHostToDevice);
+        return d_mat;
+}
+
+matrix* hostMultMat(matrix *a, matrix *b){
+    matrix *result = new matrix(a->row,b->col);
+    matrix *d_result = copyMatrixDev(result);
+    matrix *d_a = copyMatrixDev(a);
+    matrix *d_b = copyMatrixDev(b);
+
+    cudaDeviceSynchronize();
+    mat_mult<<<1,1>>>(d_a,d_b,d_result);
+
+    cudaDeviceSynchronize();
+    return d_result;
+
+}
+
+
 int main(){
 
     matrix **mat_arr = initialize();
-    matrix *d_mat[MAT_COUNT];
+//    matrix *d_mat[MAT_COUNT];
+//    double *mat_data[MAT_COUNT];
 
-    for(int i = 0; i < MAT_COUNT; i++){
+/*    for(int i = 0; i < MAT_COUNT; i++){
 
         cudaMalloc(&d_mat[i], sizeof(matrix));
         cudaMemcpy(d_mat[i], mat_arr[i], sizeof(matrix),
                 cudaMemcpyHostToDevice);
-        double *mat_data;
-        cudaMalloc(&mat_data, sizeof(double) * mat_arr[i]->col * mat_arr[i]->row);
-        cudaMemcpy(mat_data, mat_arr[i]->data, sizeof(double) * mat_arr[i]->col * mat_arr[i]->row,
+        cudaMalloc(&mat_data[i], sizeof(double) * mat_arr[i]->col * mat_arr[i]->row);
+        cudaMemcpy(mat_data[i], mat_arr[i]->data, sizeof(double) * mat_arr[i]->col * mat_arr[i]->row,
                 cudaMemcpyHostToDevice);
-        cudaMemcpy(&(d_mat[i]->data),&mat_data, sizeof(double *),
+        cudaMemcpy(&(d_mat[i]->data),&mat_data[i], sizeof(double *),
                 cudaMemcpyHostToDevice);
-        printMat(mat_arr[i]);
-        d_printMat<<<1,1>>>(d_mat[i]);
-    }
+    //    printMat(mat_arr[i]);
+    //    d_printMat<<<1,1>>>(d_mat[i]);
+    } */
+    matrix *d_result = hostMultMat(mat_arr[0],mat_arr[1]);
+
+    d_printMat<<<1,1>>>(d_result);
 
     cudaDeviceSynchronize();
 
