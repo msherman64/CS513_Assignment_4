3129c15d1127c90c9af631f94f2da4b12ab09321
Michael Sherman <msherman@winlab.rutgers.edu>
2017-05-07 01:10:10 -0400

    added generation file

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+all: gen multiply
 multiply: managed.cu
 	nvcc -o multiply managed.cu
+gen: gen_file.cu
+	nvcc -o gen_file gen_file.cu
 clean:
-	rm multiply
+	rm multiply gen_file
---
+++ gen_file.cu
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+//constant for architecture
+int SEED = 15; //seed for rand
+int DIM_LIM = 32; //max size of a matrix
+double INIT_VAL = 0.06; //initial value of matrix
+int MAT_COUNT = 100000; //
+
+
+/* file format
+
+
+ndicate the size of array A)
+n_1 n_2 n_3... n_k (k numbers in a single line indicate the dimensions of matrices)
+n_1 by n_2 matrix 1 (n_1 rows, each row contains n_2 doubles)
+n_2 by n_3 matrix 2 (n_2 rows, each row contains n_3 doubles)
+...
+n_{k-1} by n_k matrix k-1
+
+*/
+
+
+
+double randMToN(double M, double N)
+{
+    return M + (rand() / ( RAND_MAX / (N-M) ) ) ;  
+}
+
+void gen_matrix(int rows, int cols, FILE *fp){
+    for(int i = 0; i < rows; i++){//each row as outer loop
+        for(int j = 0; j < cols; j++){ //each element in row, across all columns
+            fprintf(fp, "%f ", floor(randMToN(1,10))); //print space after each value
+            //fprintf(fp, "%f ", INIT_VAL); //print space after each value
+        }
+        //print newline after each row.
+        fprintf(fp, "\n");
+    }
+}
+
+
+
+
+
+
+
+int main(int argc, char *argv[]){
+	
+    //initialize random number gen, get array sizes
+    srand(SEED); //init random gen
+    int dim[MAT_COUNT + 1]; //stores matrix sizes
+    for(int z = 0; z <= MAT_COUNT; z++){
+        dim[z] = rand()%DIM_LIM + 1;//random between 1 and limit
+    }
+
+    const char* filename = "input.txt";
+    FILE *fp = fopen(filename, "w");
+    fprintf(fp, "%d\n", MAT_COUNT + 1); //count of arrays
+    for(int i = 0; i < MAT_COUNT + 1; i++){
+        fprintf(fp, "%d ", dim[i]);
+    }
+
+    for(int i = 0; i < MAT_COUNT; i++){
+        gen_matrix(dim[i], dim[i+1], fp);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
